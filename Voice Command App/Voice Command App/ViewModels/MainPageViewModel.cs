3d740d263071029161b62492a9930fee4a6dd561
Ancita Andrade <ancita.andrade.ext@sivantos.com>
2020-05-20 15:31:23 +0530

    Added ViewModel for MainPage

---
+++ Voice Command App/Voice Command App/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Voice_Command_App.ViewModels
+{
+    public class MainPageViewModel
+    {
+        public ICommand ButtonClickCommand { get; set; }
+
+        public string MainPageLabel { get; set; }
+        public MainPageViewModel()
+        {
+            ButtonClickCommand = new Command(OnGotoNextPageButtonClicked);
+            MainPageLabel = "Welcome to Voice based App!";
+        }
+
+        private void OnGotoNextPageButtonClicked()
+        {
+            System.Diagnostics.Debug.WriteLine("hey");
+        }
+    }
+}
