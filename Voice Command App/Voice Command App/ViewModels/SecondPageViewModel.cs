fc89499d8cba4b63fa54beb07530135c37f0ab60
Mahima Bhat <bhatmahima01@gmail.com>
2020-05-20 16:59:30 +0530

    Added VM for SecondPage

---
+++ Voice Command App/Voice Command App/ViewModels/SecondPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Voice_Command_App.ViewModels
+{
+    class SecondPageViewModel
+{
+        public string SecondPageLabel { get; set; }
+        public SecondPageViewModel()
+        {
+           
+           SecondPageLabel = "Welcome to Second Page!";
+        }
+    }
+}
